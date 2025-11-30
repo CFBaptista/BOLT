1899e2a80856356234113c6b8570da4ad772a836
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-11-30 20:53:12 +0100

    Add unit tests

--- test/lbm/LatticeModelBase.cpp
+++ test/lbm/LatticeModelBase.cpp
@@ -95,12 +95,42 @@ public:
     }
 };
 
-class InvalidDimension : LatticeModelBase<InvalidDimension>
+class IntDimension : LatticeModelBase<IntDimension>
 {
 public:
     using Real = double;
 
-    static constexpr std::size_t dimension_ = 5;
+    static constexpr int dimension_ = 2;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> int
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, size_>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, size_>
+    {
+        return {};
+    }
+};
+
+class ZeroDimensional : LatticeModelBase<ZeroDimensional>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 0;
 
     static constexpr std::size_t size_ = 7;
 
@@ -110,6 +140,66 @@ public:
     }
 
     static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, size_>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, size_>
+    {
+        return {};
+    }
+};
+
+class FourDimensional : LatticeModelBase<FourDimensional>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 4;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, size_>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, size_>
+    {
+        return {};
+    }
+};
+
+class IntSize : LatticeModelBase<IntSize>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 2;
+
+    static constexpr int size_ = 0;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> int
     {
         return size_;
     }
@@ -155,6 +245,126 @@ public:
     }
 };
 
+class InvalidDimensionVelocity : LatticeModelBase<InvalidDimensionVelocity>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 2;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, 3>, size_>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, size_>
+    {
+        return {};
+    }
+};
+
+class InvalidSizeVelocity : LatticeModelBase<InvalidSizeVelocity>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 2;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, 3>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, size_>
+    {
+        return {};
+    }
+};
+
+class InvalidSizeWeight : LatticeModelBase<InvalidSizeWeight>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 2;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, size_>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<Real, 3>
+    {
+        return {};
+    }
+};
+
+class InvalidFloatingPointWeight : LatticeModelBase<InvalidFloatingPointWeight>
+{
+public:
+    using Real = double;
+
+    static constexpr std::size_t dimension_ = 2;
+
+    static constexpr std::size_t size_ = 7;
+
+    static constexpr auto dimension() -> std::size_t
+    {
+        return dimension_;
+    }
+
+    static constexpr auto size() -> std::size_t
+    {
+        return size_;
+    }
+
+    static constexpr auto velocities() -> std::array<std::array<int, dimension_>, 3>
+    {
+        return {};
+    }
+
+    static constexpr auto weights() -> std::array<float, size_>
+    {
+        return {};
+    }
+};
+
 static_assert(
     LatticeModel<ValidLatticeModel>,
     "ValidLatticeModel should satisfy LatticeModel concept"
@@ -167,12 +377,41 @@ static_assert(
     "InvalidValueType should not satisfy LatticeModel concept"
 );
 
+static_assert(!LatticeModel<IntDimension>, "IntDimension should not satisfy LatticeModel concept");
+
 static_assert(
-    !LatticeModel<InvalidDimension>,
-    "InvalidDimension should not satisfy LatticeModel concept"
+    !LatticeModel<ZeroDimensional>,
+    "ZeroDimensional should not satisfy LatticeModel concept"
 );
 
+static_assert(
+    !LatticeModel<FourDimensional>,
+    "FourDimensional should not satisfy LatticeModel concept"
+);
+
+static_assert(!LatticeModel<IntSize>, "IntSize should not satisfy LatticeModel concept");
+
 static_assert(!LatticeModel<InvalidSize>, "InvalidSize should not satisfy LatticeModel concept");
+
+static_assert(
+    !LatticeModel<InvalidDimensionVelocity>,
+    "InvalidDimensionVelocity should not satisfy LatticeModel concept"
+);
+
+static_assert(
+    !LatticeModel<InvalidSizeVelocity>,
+    "InvalidSizeVelocity should not satisfy LatticeModel concept"
+);
+
+static_assert(
+    !LatticeModel<InvalidSizeWeight>,
+    "InvalidSizeWeight should not satisfy LatticeModel concept"
+);
+
+static_assert(
+    !LatticeModel<InvalidFloatingPointWeight>,
+    "InvalidFloatingPointWeight should not satisfy LatticeModel concept"
+);
 
 TEST_CASE("Dummy test for LatticeModel concept")
 {
