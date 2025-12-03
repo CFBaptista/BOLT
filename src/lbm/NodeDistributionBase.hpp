0df5ef7b7f0e845d365071a1a9f745fca072321b
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-12-03 21:00:52 +0100

    Add docstring

--- src/lbm/NodeDistributionBase.hpp
+++ src/lbm/NodeDistributionBase.hpp
@@ -2,9 +2,21 @@
 
 #include <type_traits>
 
+/**
+ * @class NodeDistributionBase
+ * @brief Class that serves as a base for distribution functions at lattice nodes.
+ */
 class NodeDistributionBase
 {
 };
 
+/**
+ * @concept NodeDistribution
+ * @brief Concept that checks if a type is derived from NodeDistributionBase.
+ *
+ * This concept checks if a given type `Derived` is derived from the `NodeDistributionBase` class.
+ *
+ * @tparam Derived The type to be checked.
+ */
 template <typename Derived>
 concept NodeDistribution = requires { std::is_base_of_v<NodeDistributionBase, Derived>; };
