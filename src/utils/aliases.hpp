9bdbf071029c672b058109d8a8726965091685b8
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-12-12 01:43:47 +0100

    Add type alias for default floating point type

--- src/utils/aliases.hpp
+++ src/utils/aliases.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <array>
+#include <concepts>
 #include <cstddef>
 
 using Count = std::size_t;
 
 using Index = int;
+
+#ifndef REAL_TYPE
+#define REAL_TYPE double
+#endif
+
+using Real = REAL_TYPE;
+
+static_assert(std::floating_point<Real>, "Real must be a floating-point type");
 
 template <typename Type, std::size_t Size>
 using Vector = std::array<Type, Size>;
