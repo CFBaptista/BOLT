795d6d9fdf7d807984d9f4fe7a4c1cf40efb7770
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-12-13 21:59:42 +0100

    Test

--- test/lbm/D1Q3.cpp
+++ test/lbm/D1Q3.cpp
@@ -34,7 +34,7 @@ SCENARIO("D1Q3 velocity set properties")
 
             THEN("The dimension is 1")
             {
-                REQUIRE((dimension == expectedDimension));
+                REQUIRE((dimension == 2));
             }
         }
 
