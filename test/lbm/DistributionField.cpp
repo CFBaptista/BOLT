bebaa81fc0c92eb142fe2c1a6402812ae55cdbb4
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-05-17 16:01:35 +0200

    Update tests

--- test/lbm/DistributionField.cpp
+++ test/lbm/DistributionField.cpp
@@ -4,91 +4,94 @@
 
 #include "lbm/DistributionField.hpp"
 
-SCENARIO("Assert static member data of DistributionField")
+SCENARIO("Verify intrinsic properties of a distribution field with specified shape")
 {
-    GIVEN("The node count in the x direction")
+    GIVEN("A node count in a single direction")
     {
-        const std::size_t nodeCountX = 3;
+        const std::size_t nx = 3;
 
-        THEN("The dimension of the DistributionField equals 1")
+        THEN("The dimension of the distribution field equals 1")
         {
-            REQUIRE(DistributionField<nodeCountX>::dimension == 1);
+            REQUIRE(DistributionField<nx>::dimension == 1);
         }
 
-        THEN("The size of the DistributionField equals the product of the node counts")
+        THEN("The size of the distribution field equals the node count")
         {
-            REQUIRE(DistributionField<nodeCountX>::size == nodeCountX);
+            REQUIRE(DistributionField<nx>::size == nx);
         }
 
-        THEN("The shape of the DistributionField equals the node count")
+        THEN("The shape of the distribution field equals the node count")
         {
-            REQUIRE(DistributionField<nodeCountX>::shape[0] == nodeCountX);
+            REQUIRE(DistributionField<nx>::shape[0] == nx);
         }
     }
 
-    GIVEN("The node count in the x and y directions")
+    GIVEN("Node counts in two directions")
     {
-        const std::size_t nodeCountX = 3;
-        const std::size_t nodeCountY = 4;
+        const std::size_t nx = 3;
+        const std::size_t ny = 4;
 
-        THEN("The dimension of the DistributionField equals 2")
+        THEN("The dimension of the distribution field equals 2")
         {
-            REQUIRE(DistributionField<nodeCountX, nodeCountY>::dimension == 2);
+            REQUIRE(DistributionField<ny, nx>::dimension == 2);
         }
 
-        THEN("The size of the DistributionField equals the product of the node counts")
+        THEN(
+            "The size of the distribution field equals the product of the node counts in each "
+            "direction"
+        )
         {
-            REQUIRE(DistributionField<nodeCountY, nodeCountX>::size == nodeCountX * nodeCountY);
+            REQUIRE(DistributionField<ny, nx>::size == nx * ny);
         }
 
-        THEN("The shape of the DistributionField equals the node counts")
+        THEN("The shape of the distribution field equals the node counts in each direction")
         {
-            REQUIRE(DistributionField<nodeCountY, nodeCountX>::shape[0] == nodeCountY);
-            REQUIRE(DistributionField<nodeCountY, nodeCountX>::shape[1] == nodeCountX);
+            REQUIRE(DistributionField<ny, nx>::shape[0] == ny);
+            REQUIRE(DistributionField<ny, nx>::shape[1] == nx);
         }
     }
 
-    GIVEN("The node count in the x, y and z directions")
+    GIVEN("Node counts in three directions")
     {
-        const std::size_t nodeCountX = 3;
-        const std::size_t nodeCountY = 4;
-        const std::size_t nodeCountZ = 5;
+        const std::size_t nx = 3;
+        const std::size_t ny = 4;
+        const std::size_t nz = 5;
 
-        THEN("The dimension of the DistributionField equals 3")
+        THEN("The dimension of the distribution field equals 3")
         {
-            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::dimension == 3);
+            REQUIRE(DistributionField<nz, ny, nx>::dimension == 3);
         }
 
-        THEN("The size of the DistributionField equals the product of the node counts")
+        THEN(
+            "The size of the distribution field equals the product of the node counts in each "
+            "direction"
+        )
         {
-            REQUIRE(
-                DistributionField<nodeCountZ, nodeCountY, nodeCountX>::size ==
-                nodeCountX * nodeCountY * nodeCountZ
-            );
+            REQUIRE(DistributionField<nz, ny, nx>::size == nx * ny * nz);
         }
 
-        THEN("The shape of the DistributionField equals the node counts")
+        THEN("The shape of the distribution field equals the node counts in each direction")
         {
-            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[0] == nodeCountZ);
-            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[1] == nodeCountY);
-            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[2] == nodeCountX);
+            REQUIRE(DistributionField<nz, ny, nx>::shape[0] == nz);
+            REQUIRE(DistributionField<nz, ny, nx>::shape[1] == ny);
+            REQUIRE(DistributionField<nz, ny, nx>::shape[2] == nx);
         }
     }
 }
 
-SCENARIO("Linear and Cartesian indexing of a DistributionField are consistent")
+SCENARIO("Linear and Cartesian indexing of a distribution field is consistent")
 {
-    GIVEN("A 2D DistributionField with node counts in the x and y directions")
+    GIVEN("A 2D distribution field")
     {
-        const std::size_t nodeCountX = 3;
-        const std::size_t nodeCountY = 4;
-        DistributionField<nodeCountY, nodeCountX> distributionField;
+        const std::size_t nx = 3;
+        const std::size_t ny = 4;
+        DistributionField<ny, nx> distributionField;
 
         WHEN("Assigning a value to each node using linear indexing")
         {
-            for (std::size_t index = 0; index < nodeCountX * nodeCountY; ++index)
+            for (std::size_t i = 0; i < nx * ny; ++i)
             {
-                distributionField[index] = static_cast<double>(index);
+                distributionField[i] = static_cast<double>(i);
             }
 
             THEN(
@@ -96,14 +99,13 @@ SCENARIO("Linear and Cartesian indexing of a DistributionField are consistent")
                 "value"
             )
             {
-                for (std::size_t indexY = 0; indexY < nodeCountY; ++indexY)
+                for (std::size_t iy = 0; iy < ny; ++iy)
                 {
-                    for (std::size_t indexX = 0; indexX < nodeCountX; ++indexX)
+                    for (std::size_t ix = 0; ix < nx; ++ix)
                     {
-                        const std::size_t linearIndex = (indexY * nodeCountX) + indexX;
-                        REQUIRE(
-                            distributionField[linearIndex] == distributionField[indexY, indexX]
-                        );
+                        const std::size_t i = (iy * nx) + ix;
+
+                        REQUIRE(distributionField[i] == distributionField[iy, ix]);
                     }
                 }
             }
@@ -111,11 +113,11 @@ SCENARIO("Linear and Cartesian indexing of a DistributionField are consistent")
 
         WHEN("Assigning a value to each node using Cartesian indexing")
         {
-            for (std::size_t indexY = 0; indexY < nodeCountY; ++indexY)
+            for (std::size_t iy = 0; iy < ny; ++iy)
             {
-                for (std::size_t indexX = 0; indexX < nodeCountX; ++indexX)
+                for (std::size_t ix = 0; ix < nx; ++ix)
                 {
-                    distributionField[indexY, indexX] = static_cast<double>(indexX + indexY);
+                    distributionField[iy, ix] = static_cast<double>(ix + iy);
                 }
             }
 
@@ -124,11 +126,76 @@ SCENARIO("Linear and Cartesian indexing of a DistributionField are consistent")
                 "value"
             )
             {
-                for (std::size_t index = 0; index < nodeCountX * nodeCountY; ++index)
+                for (std::size_t i = 0; i < nx * ny; ++i)
                 {
-                    const std::size_t indexY = index / nodeCountX;
-                    const std::size_t indexX = index % nodeCountX;
-                    REQUIRE(distributionField[index] == distributionField[indexY, indexX]);
+                    const std::size_t ix = i % nx;
+                    const std::size_t iy = i / nx;
+
+                    REQUIRE(distributionField[i] == distributionField[iy, ix]);
+                }
+            }
+        }
+    }
+
+    GIVEN("A 3D DistributionField")
+    {
+        const std::size_t nx = 3;
+        const std::size_t ny = 4;
+        const std::size_t nz = 5;
+        DistributionField<nz, ny, nx> distributionField;
+
+        WHEN("Assigning a value to each node using linear indexing")
+        {
+            for (std::size_t i = 0; i < nx * ny * nz; ++i)
+            {
+                distributionField[i] = static_cast<double>(i);
+            }
+
+            THEN(
+                "The value at each node accessed using Cartesian indexing matches the assigned "
+                "value"
+            )
+            {
+                for (std::size_t iz = 0; iz < nz; ++iz)
+                {
+                    for (std::size_t iy = 0; iy < ny; ++iy)
+                    {
+                        for (std::size_t ix = 0; ix < nx; ++ix)
+                        {
+                            const std::size_t i = (iz * ny * nx) + (iy * nx) + ix;
+
+                            REQUIRE(distributionField[i] == distributionField[iz, iy, ix]);
+                        }
+                    }
+                }
+            }
+        }
+
+        WHEN("Assigning a value to each node using Cartesian indexing")
+        {
+            for (std::size_t iz = 0; iz < nz; ++iz)
+            {
+                for (std::size_t iy = 0; iy < ny; ++iy)
+                {
+                    for (std::size_t ix = 0; ix < nx; ++ix)
+                    {
+                        distributionField[iz, iy, ix] = static_cast<double>(ix + iy + iz);
+                    }
+                }
+            }
+
+            THEN(
+                "The value at each node accessed using linear indexing matches the assigned "
+                "value"
+            )
+            {
+                for (std::size_t i = 0; i < nx * ny * nz; ++i)
+                {
+                    const std::size_t ix = i % nx;
+                    const std::size_t iy = (i / nx) % ny;
+                    const std::size_t iz = i / (ny * nx);
+
+                    REQUIRE(distributionField[i] == distributionField[iz, iy, ix]);
                 }
             }
         }
