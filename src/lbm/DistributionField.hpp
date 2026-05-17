10a3777ca01fd6104508551d352343db48fa1c88
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-05-17 06:00:21 +0200

    Add linear and Cartesian indexing

--- src/lbm/DistributionField.hpp
+++ src/lbm/DistributionField.hpp
@@ -2,6 +2,8 @@
 
 #include <array>
 #include <cstddef>
+#include <mdspan>
+#include <vector>
 
 template <std::size_t... Shape>
 class DistributionField
@@ -13,4 +15,32 @@ public:
 
     static_assert(size > 0, "A DistributionField must have a positive size.");
     static_assert(dimension > 0 && dimension < 4, "A DistributionField must be 1D, 2D or 3D.");
+
+    DistributionField() : data_(size), view_(data_.data()) {};
+
+    auto operator[](std::size_t index) -> double&
+    {
+        return data_[index];
+    }
+
+    auto operator[](std::size_t index) const -> const double&
+    {
+        return data_[index];
+    }
+
+    template <typename... Indices>
+    auto operator[](Indices... indices) -> double&
+    {
+        return view_[indices...];
+    }
+
+    template <typename... Indices>
+    auto operator[](Indices... indices) const -> const double&
+    {
+        return view_[indices...];
+    }
+
+private:
+    std::vector<double> data_;
+    std::mdspan<double, std::extents<std::size_t, Shape...>> view_;
 };
--- test/lbm/DistributionField.cpp
+++ test/lbm/DistributionField.cpp
@@ -38,13 +38,13 @@ SCENARIO("Assert static member data of DistributionField")
 
         THEN("The size of the DistributionField equals the product of the node counts")
         {
-            REQUIRE(DistributionField<nodeCountX, nodeCountY>::size == nodeCountX * nodeCountY);
+            REQUIRE(DistributionField<nodeCountY, nodeCountX>::size == nodeCountX * nodeCountY);
         }
 
         THEN("The shape of the DistributionField equals the node counts")
         {
-            REQUIRE(DistributionField<nodeCountX, nodeCountY>::shape[0] == nodeCountX);
-            REQUIRE(DistributionField<nodeCountX, nodeCountY>::shape[1] == nodeCountY);
+            REQUIRE(DistributionField<nodeCountY, nodeCountX>::shape[0] == nodeCountY);
+            REQUIRE(DistributionField<nodeCountY, nodeCountX>::shape[1] == nodeCountX);
         }
     }
 
@@ -56,22 +56,81 @@ SCENARIO("Assert static member data of DistributionField")
 
         THEN("The dimension of the DistributionField equals 3")
         {
-            REQUIRE(DistributionField<nodeCountX, nodeCountY, nodeCountZ>::dimension == 3);
+            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::dimension == 3);
         }
 
         THEN("The size of the DistributionField equals the product of the node counts")
         {
             REQUIRE(
-                DistributionField<nodeCountX, nodeCountY, nodeCountZ>::size ==
+                DistributionField<nodeCountZ, nodeCountY, nodeCountX>::size ==
                 nodeCountX * nodeCountY * nodeCountZ
             );
         }
 
         THEN("The shape of the DistributionField equals the node counts")
         {
-            REQUIRE(DistributionField<nodeCountX, nodeCountY, nodeCountZ>::shape[0] == nodeCountX);
-            REQUIRE(DistributionField<nodeCountX, nodeCountY, nodeCountZ>::shape[1] == nodeCountY);
-            REQUIRE(DistributionField<nodeCountX, nodeCountY, nodeCountZ>::shape[2] == nodeCountZ);
+            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[0] == nodeCountZ);
+            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[1] == nodeCountY);
+            REQUIRE(DistributionField<nodeCountZ, nodeCountY, nodeCountX>::shape[2] == nodeCountX);
+        }
+    }
+}
+
+SCENARIO("Linear and Cartesian indexing of a DistributionField are consistent")
+{
+    GIVEN("A 2D DistributionField with node counts in the x and y directions")
+    {
+        const std::size_t nodeCountX = 3;
+        const std::size_t nodeCountY = 4;
+        DistributionField<nodeCountY, nodeCountX> distributionField;
+
+        WHEN("Assigning a value to each node using linear indexing")
+        {
+            for (std::size_t index = 0; index < nodeCountX * nodeCountY; ++index)
+            {
+                distributionField[index] = static_cast<double>(index);
+            }
+
+            THEN(
+                "The value at each node accessed using Cartesian indexing matches the assigned "
+                "value"
+            )
+            {
+                for (std::size_t indexY = 0; indexY < nodeCountY; ++indexY)
+                {
+                    for (std::size_t indexX = 0; indexX < nodeCountX; ++indexX)
+                    {
+                        const std::size_t linearIndex = (indexY * nodeCountX) + indexX;
+                        REQUIRE(
+                            distributionField[linearIndex] == distributionField[indexY, indexX]
+                        );
+                    }
+                }
+            }
+        }
+
+        WHEN("Assigning a value to each node using Cartesian indexing")
+        {
+            for (std::size_t indexY = 0; indexY < nodeCountY; ++indexY)
+            {
+                for (std::size_t indexX = 0; indexX < nodeCountX; ++indexX)
+                {
+                    distributionField[indexY, indexX] = static_cast<double>(indexX + indexY);
+                }
+            }
+
+            THEN(
+                "The value at each node accessed using linear indexing matches the assigned "
+                "value"
+            )
+            {
+                for (std::size_t index = 0; index < nodeCountX * nodeCountY; ++index)
+                {
+                    const std::size_t indexY = index / nodeCountX;
+                    const std::size_t indexX = index % nodeCountX;
+                    REQUIRE(distributionField[index] == distributionField[indexY, indexX]);
+                }
+            }
         }
     }
 }
