b55cbfc32a85814e13e3814b4dfb8db301cc1f9d
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-05-26 23:51:53 +0000

    Update docstrings

--- src/core/time/SimulationTime.hpp
+++ src/core/time/SimulationTime.hpp
@@ -9,25 +9,56 @@ namespace bolt
 namespace core
 {
 
+/**
+ * @brief A class template representing the time for a numerical simulation.
+ *
+ * This class manages the state of simulation time and provides methods to obtain the current time
+ * and to advance time by a time step.
+ *
+ * @tparam Real The floating-point type to be used for representing time.
+ */
 template <std::floating_point Real>
 class SimulationTime
 {
 public:
     using value_type = Real;
 
+    /**
+     * @brief Constructor to initialize the simulation time.
+     *
+     * @param starting_time The initial time of the simulation.
+     * @param time_step The time step to advance the simulation at each step.
+     * @param number_of_steps The total number of steps in the simulation.
+     */
     SimulationTime(
         const Real& starting_time,
         const Real& time_step,
         const std::size_t& number_of_steps
     );
+    /**
+     * @brief Destructor.
+     */
     ~SimulationTime() = default;
     SimulationTime(const SimulationTime&) = delete;
     auto operator=(const SimulationTime&) -> SimulationTime& = delete;
     SimulationTime(SimulationTime&&) noexcept = delete;
     auto operator=(SimulationTime&&) noexcept -> SimulationTime& = delete;
 
+    /**
+     * @brief Get the current time of the simulation.
+     *
+     * @return The current time of the simulation.
+     */
     auto current_time() const -> Real;
+    /**
+     * @brief Get the end time of the simulation.
+     *
+     * @return The end time of the simulation.
+     */
     auto end_time() const -> Real;
+    /**
+     * @brief Advance the simulation time by one time step.
+     */
     auto advance() -> void;
 
 private:
