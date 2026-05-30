e6003617330c9632241a24da88ef8facded9a8dc
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-05-30 16:07:47 +0000

    Add command-line options, configuration file parsing and logging

--- apps/solver/main.cpp
+++ apps/solver/main.cpp
@@ -1,26 +1,117 @@
-#include <cstddef>
+#include <chrono>
 #include <filesystem>
-#include <iostream>
-#include <span>
+#include <format>
+#include <string>
+#include <utility>
 
+#include "CLI/CLI.hpp"
+#include "quill/Backend.h"
+#include "quill/Frontend.h"
+#include "quill/LogMacros.h"
+#include "quill/Logger.h"
+#include "quill/core/LogLevel.h"
+#include "quill/sinks/ConsoleSink.h"
+#include "quill/sinks/FileSink.h"
 #include <toml++/toml.hpp>
 
 auto main(int argc, char* argv[]) -> int
 {
-    const std::span args = std::span(argv, static_cast<std::size_t>(argc));
+    const auto now = std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now());
+    const auto timestamp = std::format("{:%Y%m%d-%H%M%S}", now);
+
+    CLI::App app{"BOLT (Boltzmann Operator Lattice Toolkit) is a C++ library for Computational "
+                 "Fluid Dynamics based on the Lattice Boltzmann Method."};
+
+    std::filesystem::path config_file;
+    app.add_option(
+           "-c,--config_file", config_file,
+           "Path to a TOML configuration file for running a simulation with BOLT"
+    )
+        ->required();
+
+    std::filesystem::path output_directory = ".";
+    app.add_option(
+           "-o,--output_directory", output_directory,
+           "Path to the output directory for logs and simulation results"
+    )
+        ->default_str(output_directory);
+
+    std::string log_level = "info";
+    app.add_option(
+           "-l,--log_level", log_level,
+           "Logging level (trace, debug, info, warning, error, critical)"
+    )
+        ->default_str(log_level);
+
+    CLI11_PARSE(app, argc, argv);
+
+    quill::Backend::start();
+
+    auto console_sink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("console");
+    auto file_sink = quill::Frontend::create_or_get_sink<quill::FileSink>(
+        output_directory / std::format("bolt_{}.log", timestamp)
+    );
+
+    quill::Logger* logger = quill::Frontend::create_or_get_logger(
+        "quill", {std::move(console_sink), std::move(file_sink)}
+    );
+
+    if (log_level == "trace_l3")
+    {
+        logger->set_log_level(quill::LogLevel::TraceL3);
+    }
+    else if (log_level == "trace_l2")
+    {
+        logger->set_log_level(quill::LogLevel::TraceL2);
+    }
+    else if (log_level == "trace_l1")
+    {
+        logger->set_log_level(quill::LogLevel::TraceL1);
+    }
+    else if (log_level == "debug")
+    {
+        logger->set_log_level(quill::LogLevel::Debug);
+    }
+    else if (log_level == "info")
+    {
+        logger->set_log_level(quill::LogLevel::Info);
+    }
+    else if (log_level == "notice")
+    {
+        logger->set_log_level(quill::LogLevel::Notice);
+    }
+    else if (log_level == "warning")
+    {
+        logger->set_log_level(quill::LogLevel::Warning);
+    }
+    else if (log_level == "error")
+    {
+        logger->set_log_level(quill::LogLevel::Error);
+    }
+    else if (log_level == "critical")
+    {
+        logger->set_log_level(quill::LogLevel::Critical);
+    }
+    else
+    {
+        LOG_ERROR(logger, "Invalid log level specified: {}", log_level);
+        return 1;
+    }
+    LOG_INFO(logger, "Log level set to {}", log_level);
 
     toml::table configuration;
 
     try
     {
-        const std::filesystem::path configuration_filepath = args[1];
-        configuration = toml::parse_file(configuration_filepath.string());
+        LOG_INFO(logger, "Parsing configuration file");
 
-        std::cout << configuration << "\n";
+        configuration = toml::parse_file(config_file.string());
+
+        LOG_INFO(logger, "Configuration file parsed successfully");
     }
-    catch (const toml::parse_error& err)
+    catch (const toml::parse_error& e)
     {
-        std::cerr << "Parsing the simulation configuration file failed:\n" << err << "\n";
+        LOG_ERROR(logger, "Failed to parse configuration file:\n{}", e.what());
         return 1;
     }
 
