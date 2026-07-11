8cec5732085be779e8a291fe5d05c948a64bf984
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-07-11 01:44:00 +0000

    Update unit tests

--- test/apps/solver/configuration.cpp
+++ test/apps/solver/configuration.cpp
@@ -1,14 +1,15 @@
-#include <array>
 #include <filesystem>
 #include <fstream>
 #include <span>
 #include <string>
+#include <vector>
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include "solver/configuration.hpp"
 
-SCENARIO("Configuration file is read correctly")
+SCENARIO("Application configuration is constructed correctly")
 {
     GIVEN("A valid configuration file")
     {
@@ -21,24 +22,64 @@ SCENARIO("Configuration file is read correctly")
         )";
         std::ofstream(config_file) << config_content;
 
-        std::string program_name = "./bolt";
-        std::string config_flag = "--config_file";
-        std::string config_argument = config_file.string();
-        std::array<char*, 3> argv = {
-            program_name.data(), config_flag.data(), config_argument.data()
-        };
-        std::span<char*> args{argv};
-
-        WHEN("The configuration file is parsed")
+        GIVEN("Only mandatory command line options are provided")
         {
-            const ApplicationConfiguration config = parse_configuration(args);
+            std::string program_name = "./bolt";
+            std::string config_flag = "--config_file";
+            std::string config_argument = config_file.string();
 
-            THEN("The configuration values are read correctly")
+            std::vector<char*> argv = {
+                program_name.data(), config_flag.data(), config_argument.data()
+            };
+            std::span<char*> args{argv};
+
+            WHEN("The configuration file is parsed")
             {
-                REQUIRE(config.io.configuration_file == config_file.string());
-                REQUIRE(config.time.start_time == 0.0);
-                REQUIRE(config.time.time_step == 0.1);
-                REQUIRE(config.time.number_of_steps == 10);
+                const ApplicationConfiguration config = parse_configuration(args);
+
+                THEN("The configuration values are read correctly")
+                {
+                    REQUIRE(config.io.configuration_file == config_file.string());
+                    REQUIRE(config.io.output_directory == ".");
+                    REQUIRE(config.io.log_level == "info");
+
+                    REQUIRE(config.time.start_time == 0.0);
+                    REQUIRE(config.time.time_step == 0.1);
+                    REQUIRE(config.time.number_of_steps == 10);
+                }
+            }
+        }
+
+        GIVEN("Mandatory and optional command line options are provided")
+        {
+            std::string program_name = "./bolt";
+            std::string config_flag = "--config_file";
+            std::string config_argument = config_file.string();
+            std::string output_flag = "--output_directory";
+            std::string output_argument = "/tmp/output";
+            std::string log_level_flag = "--log_level";
+            std::string log_level_argument = GENERATE(
+                "trace_l3", "trace_l2", "trace_l1", "debug", "info", "notice", "warning", "error",
+                "critical"
+            );
+
+            std::vector<char*> argv = {program_name.data(),      config_flag.data(),
+                                       config_argument.data(),   output_flag.data(),
+                                       output_argument.data(),   log_level_flag.data(),
+                                       log_level_argument.data()};
+            std::span<char*> args{argv};
+
+            std::filesystem::create_directory(output_argument);
+
+            WHEN("The configuration file is parsed")
+            {
+                const ApplicationConfiguration config = parse_configuration(args);
+
+                THEN("The configuration values are read correctly")
+                {
+                    REQUIRE(config.io.output_directory == output_argument);
+                    REQUIRE(config.io.log_level == log_level_argument);
+                }
             }
         }
 
