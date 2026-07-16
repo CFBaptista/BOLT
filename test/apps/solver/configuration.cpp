867eba1b746afaf7584857c201a49e6cf002caca
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2026-07-16 06:20:54 +0000

    Update test

--- test/apps/solver/configuration.cpp
+++ test/apps/solver/configuration.cpp
@@ -1,4 +1,7 @@
 #include <filesystem>
+#include <string>
+#include <string_view>
+#include <vector>
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -8,15 +11,18 @@ SCENARIO("Reading a valid configuration file does not throw an exception")
 {
     GIVEN("A valid configuration file")
     {
-        const std::filesystem::path config_file =
+        const std::filesystem::path config_filepath =
             std::filesystem::path{__FILE__}.parent_path().parent_path().parent_path() / "data" /
             "configuration.toml";
+        const std::string config_filepath_string = config_filepath.string();
+
+        const std::vector<std::string_view> args{"./bolt", "--config_file", config_filepath_string};
 
         WHEN("The configuration is read")
         {
             THEN("No exception is thrown")
             {
-                REQUIRE_NOTHROW(read_configuration_file(config_file));
+                REQUIRE_NOTHROW(parse_configuration(args));
             }
         }
     }
